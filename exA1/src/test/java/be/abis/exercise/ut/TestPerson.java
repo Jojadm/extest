0d1d293052d659089ea651f99c42b0dd0866615c
Sonja De Meersman <sonja_dm@telenet.be>
2020-10-02 11:04:50 +0200

    exA1-1

--- exA1/src/test/java/be/abis/exercise/ut/TestPerson.java
+++ exA1/src/test/java/be/abis/exercise/ut/TestPerson.java
@@ -1,11 +1,22 @@
 package be.abis.exercise.ut;
 
+import be.abis.exercise.model.Person;
 import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestPerson {
 
     @Test
     public void personShouldBe42() {
+        //arrange
+        Person personUnderTest = new Person(1, "Sonja", "De Meersman", LocalDate.of(1977, 12, 21));
+        //act
+        int result= personUnderTest.calculateAge();
+        //assert
+        assertEquals(42,result);
 
     }
 }
