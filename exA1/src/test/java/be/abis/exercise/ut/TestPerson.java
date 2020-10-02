aafde53ced4c47ccc310608d66f93d79f2822b1b
Sonja De Meersman <sonja_dm@telenet.be>
2020-10-02 11:28:43 +0200

    exA1-4

--- exA1/src/test/java/be/abis/exercise/ut/TestPerson.java
+++ exA1/src/test/java/be/abis/exercise/ut/TestPerson.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class TestPerson {
 
@@ -17,6 +19,16 @@ public class TestPerson {
         int result= personUnderTest.calculateAge();
         //assert
         assertEquals(42,result);
+
+    }
+    @Test
+    public void personShouldBe42withHamcrest() {
+        //arrange
+        Person personUnderTest = new Person(1, "Sonja", "De Meersman", LocalDate.of(1977, 12, 21));
+        //act
+        int result= personUnderTest.calculateAge();
+        //assert
+        assertThat(result, equalTo(42));
 
     }
 }
