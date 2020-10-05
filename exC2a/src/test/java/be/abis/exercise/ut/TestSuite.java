30f5b6bbbf6ed65fc4de3d22cb9141041794c8f4
Sonja De Meersman <sonja_dm@telenet.be>
2020-10-05 15:56:46 +0200

    exC2a

--- exC2a/src/test/java/be/abis/exercise/ut/TestSuite.java
+++ exC2a/src/test/java/be/abis/exercise/ut/TestSuite.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestAddress.class, TestPerson.class})
+@Suite.SuiteClasses({TestAddress.class, TestPerson.class, TestCompany.class})
 public class TestSuite {
 }
